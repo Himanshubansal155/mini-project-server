4044b771761dd765f691a360fd840da5e9bba28f
Himanshu Bansal <himanshubansal155@gmail.com>
2021-11-16 21:21:15 +0530

    added middleware for authentication

--- firebase/index.js
+++ firebase/index.js
@@ -4,7 +4,6 @@ var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // databaseURL: "https://ecommerce-225c8.firebaseio.com",
 });
 
 module.exports = admin;
